c41634fd5e632ef06235a6d839f73560c2ed3368
Ivan Franjic <2338268+frennky@users.noreply.github.com>
2022-06-22 10:56:41 +0000

    added mock implementation

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"google.golang.org/grpc"
 	"log"
 	"net"
 	pb "terrarium-grpc-gateway/pkg/terrarium"
+
+	"google.golang.org/grpc"
 )
 
 type TerrariumGrpcGateway struct {
@@ -16,50 +17,53 @@ type TerrariumGrpcGateway struct {
 }
 
 func (s *TerrariumGrpcGateway) Configure(ctx context.Context, request *pb.ModuleConfigurationRequest) (*pb.TransactionStatusResponse, error) {
-
-
-	//TODO implement me
-	panic("implement me")
+	return &pb.TransactionStatusResponse{
+		Status: pb.TransactionStatusResponse_OK,
+		StatusMessage: "All is good",
+	}, nil
 }
 
 func (s *TerrariumGrpcGateway) BeginVersion(ctx context.Context, request *pb.BeginVersionRequest) (*pb.BeginVersionResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	return &pb.BeginVersionResponse{
+		SessionKey: "1234",
+	}, nil
 }
 
 func (s *TerrariumGrpcGateway) RegisterModuleDependencies(ctx context.Context, request *pb.RegisterModuleDependenciesRequest) (*pb.TransactionStatusResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	return &pb.TransactionStatusResponse{
+		Status: pb.TransactionStatusResponse_OK,
+		StatusMessage: "All is good",
+	}, nil
 }
 
 func (s *TerrariumGrpcGateway) RegisterContainerDependencies(ctx context.Context, request *pb.RegisterContainerDependenciesRequest) (*pb.TransactionStatusResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	return &pb.TransactionStatusResponse{
+		Status: pb.TransactionStatusResponse_OK,
+		StatusMessage: "All is good",
+	}, nil
 }
 
 func (s *TerrariumGrpcGateway) UploadSourceZip(server pb.Publisher_UploadSourceZipServer) error {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (s *TerrariumGrpcGateway) EndVersion(ctx context.Context, request *pb.EndVersionRequest) (*pb.TransactionStatusResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	return &pb.TransactionStatusResponse{
+		Status: pb.TransactionStatusResponse_OK,
+		StatusMessage: "All is good",
+	}, nil
 }
 
 func (s *TerrariumGrpcGateway) DownloadSourceZip(request *pb.DownloadSourceZipRequest, server pb.Consumer_DownloadSourceZipServer) error {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (s *TerrariumGrpcGateway) RetrieveContainerDependencies(request *pb.RetrieveContainerDependenciesRequest, server pb.Consumer_RetrieveContainerDependenciesServer) error {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (s *TerrariumGrpcGateway) RetrieveModuleDependencies(request *pb.RetrieveModuleDependenciesRequest, server pb.Consumer_RetrieveModuleDependenciesServer) error {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func main() {
