2bc6283a44428f6e76a94051b4478258bea19d6f
Olivier Delannoy <odelanno@synamedia.com>
2022-06-23 13:45:08 +0200

    Generated Protocol buffer for storage
    Co-Authored-By: Ivan F. <2338268+frennky@users.noreply.github.com>

--- genproto.sh
+++ genproto.sh
@@ -1,5 +1,8 @@
 protoc --go_out=. --go_opt=module=github.com/terrariumcloud/terrarium-grpc-gateway \
     --go-grpc_out=. --go-grpc_opt=module=github.com/terrariumcloud/terrarium-grpc-gateway \
-    pkg/terrarium/module.proto internal/services/module_create.proto internal/services/module_session_manager.proto
+    pkg/terrarium/module.proto \
+    internal/services/module_create.proto \
+    internal/services/module_session_manager.proto \
+    internal/services/module_storage.proto
 
 
