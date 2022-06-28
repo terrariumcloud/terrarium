769298f519a91d0aec0f42bc053bf8df9b44adde
Ivan Franjic <2338268+frennky@users.noreply.github.com>
2022-06-28 08:12:16 +0000

    added session manager service

---
+++ cmd/session-service/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"terrarium-grpc-gateway/internal/services"
+	"terrarium-grpc-gateway/pkg/terrarium"
+
+	"google.golang.org/grpc"
+)
+
+type SessionService struct {
+	services.UnimplementedSessionManagerServer
+}
+
+func (s *SessionService) BeginVersion(ctx context.Context, request *services.BeginVersionRequest) (*terrarium.BeginVersionResponse, error) {
+	return nil, nil
+}
+
+func (s *SessionService) AbortVersion(ctx context.Context, request *services.TerminateVersionRequest) (*terrarium.TransactionStatusResponse, error) {
+	return nil, nil
+}
+
+func (s *SessionService) PublishVersion(ctx context.Context, request *services.TerminateVersionRequest) (*terrarium.TransactionStatusResponse, error) {
+	return nil, nil
+}
+
+func main() {
+	fmt.Println("Welcome to Terrarium GRPC API Gateway")
+	flag.Parse()
+	lis, err := net.Listen("tcp", "0.0.0.0:9443")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+
+	sessionServiceServer := &SessionService{}
+	// Need TLS
+	grpcServer := grpc.NewServer(opts...)
+	services.RegisterSessionManagerServer(grpcServer, sessionServiceServer)
+	grpcServer.Serve(lis)
+}
