49eedc1867329f224f412fe701b70064b1ce4170
Ivan Franjic <2338268+frennky@users.noreply.github.com>
2022-07-20 12:46:48 +0000

    updated root cmd

--- cmd/root.go
+++ cmd/root.go
@@ -18,14 +18,12 @@ var awsRegion string
 var rootCmd = &cobra.Command{
 	Use:   "terrarium",
 	Short: "Terrarium Services",
-	Long:  `Runs GRPC server that exposes Terrarium Services`,
+	Long:  "Runs GRPC server that exposes Terrarium Services",
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVarP(&address, "address", "a", defaultAddress, "IP Address (required)")
-	rootCmd.MarkPersistentFlagRequired("address")
-	rootCmd.PersistentFlags().StringVarP(&port, "port", "p", defaultPort, "Port number (required)")
-	rootCmd.MarkPersistentFlagRequired("port")
+	rootCmd.PersistentFlags().StringVarP(&address, "address", "a", defaultAddress, "IP Address")
+	rootCmd.PersistentFlags().StringVarP(&port, "port", "p", defaultPort, "Port number")
 	rootCmd.PersistentFlags().StringVarP(&awsAccessKey, "aws-access-key", "", "", "AWS Access Key (required)")
 	rootCmd.MarkPersistentFlagRequired("aws-access-key")
 	rootCmd.PersistentFlags().StringVarP(&awsSecretKey, "aws-secret-key", "", "", "AWS Secret Key (required)")
