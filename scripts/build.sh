41defdb53ef503c76533a6d54f7beeaee9e4dac0
Ivan Franjic <2338268+frennky@users.noreply.github.com>
2022-08-17 19:04:27 +0000

    updated scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-rm -rf ./vendor terrarium
 go fmt ./...
 go mod tidy
-go mod vendor
 go build -o terrarium -mod vendor
--- scripts/gen_proto.sh
+++ scripts/gen_proto.sh
@@ -3,5 +3,5 @@ protoc --go_out=. --go_opt=module=github.com/terrariumcloud/terrarium-grpc-gatew
     pb/terrarium/module/module.proto \
     pb/terrarium/module/services/registrar.proto \
     pb/terrarium/module/services/version_manager.proto \
-    pb/terrarium/module/services/dependency_resolver.proto \
+    pb/terrarium/module/services/dependency_manager.proto \
     pb/terrarium/module/services/storage.proto
