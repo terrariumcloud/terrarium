d0b8343ec3da60ac32124ca75371af4e6b469cd7
Ilija Dimitrovski <idimitrovski@synamedia.com>
2023-01-11 17:13:05 +0100

    Add test tracer

--- cmd/root.go
+++ cmd/root.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"time"
 
 	"github.com/terrariumcloud/terrarium/internal/restapi"
 
@@ -14,9 +16,11 @@ import (
 	"google.golang.org/grpc"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	stdoutTrace "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
@@ -45,6 +49,7 @@ func InitOTELTracer() (*sdktrace.TracerProvider, error) {
 	)
 	otel.SetTracerProvider(tp)
 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+
 	return tp, nil
 }
 
@@ -78,15 +83,42 @@ func startGRPCService(name string, service services.Service) {
 		log.Fatalf("Failed: %v", err)
 	}
 
-	tp, err := InitOTELTracer()
+	traceProvider, err := InitOTELTracer()
 
 	if err != nil {
 		log.Fatalf("Failed to initiate OTEL Tracer: %v", err)
 	}
 
-	if tp != nil {
-		log.Fatalf("Successfully initiated OTEL Tracer...")
+	if traceProvider != nil {
+		log.Printf("Successfully initiated OTEL Tracer...")
 	}
+
+	tracer := otel.Tracer("test-tracer")
+
+	// Attributes represent additional key-value descriptors that can be bound
+	// to a metric observer or recorder.
+	commonAttrs := []attribute.KeyValue{
+		attribute.String("attrA", "chocolate"),
+		attribute.String("attrB", "raspberry"),
+		attribute.String("attrC", "vanilla"),
+	}
+
+	// work begins
+	ctx, span := tracer.Start(
+		context.TODO(),
+		"CollectorExporter-Example",
+		trace.WithAttributes(commonAttrs...))
+	defer span.End()
+	for i := 0; i < 10; i++ {
+		_, iSpan := tracer.Start(ctx, fmt.Sprintf("Sample-%d", i))
+		log.Printf("Doing really hard work (%d / 10)\n", i+1)
+
+		<-time.After(time.Second)
+		iSpan.End()
+	}
+
+	log.Printf("Done!")
+
 }
 
 func startRESTAPIService(name, mountPath string, rootHandler restapi.RESTAPIHandler) {
@@ -94,15 +126,41 @@ func startRESTAPIService(name, mountPath string, rootHandler restapi.RESTAPIHand
 	log.Println(fmt.Sprintf("Listening on %s", endpoint))
 	log.Fatal(http.ListenAndServe(endpoint, rootHandler.GetHttpHandler(mountPath)))
 
-	tp, err := InitOTELTracer()
+	traceProvider, err := InitOTELTracer()
 
 	if err != nil {
 		log.Fatalf("Failed to initiate OTEL Tracer: %v", err)
 	}
 
-	if tp != nil {
-		log.Fatalf("Successfully initiated OTEL Tracer: %v", err)
+	if traceProvider != nil {
+		log.Printf("Successfully initiated OTEL Tracer...")
 	}
+
+	tracer := otel.Tracer("test-tracer")
+
+	// Attributes represent additional key-value descriptors that can be bound
+	// to a metric observer or recorder.
+	commonAttrs := []attribute.KeyValue{
+		attribute.String("attrA", "chocolate"),
+		attribute.String("attrB", "raspberry"),
+		attribute.String("attrC", "vanilla"),
+	}
+
+	// work begins
+	ctx, span := tracer.Start(
+		context.TODO(),
+		"CollectorExporter-Example",
+		trace.WithAttributes(commonAttrs...))
+	defer span.End()
+	for i := 0; i < 10; i++ {
+		_, iSpan := tracer.Start(ctx, fmt.Sprintf("Sample-%d", i))
+		log.Printf("Doing really hard work (%d / 10)\n", i+1)
+
+		<-time.After(time.Second)
+		iSpan.End()
+	}
+
+	log.Printf("Done!")
 }
 
 // Execute root command
