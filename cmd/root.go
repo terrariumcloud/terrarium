5fdddd20f11aab9ddbac2a8dcc75302a45874fd3
Ilija Dimitrovski <idimitrovski@synamedia.com>
2023-01-12 16:29:38 +0100

    Move OTEL collector to TagManager Publish tag function

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,10 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
-	"time"
 
 	"github.com/terrariumcloud/terrarium/internal/restapi"
 
@@ -14,14 +12,9 @@ import (
 
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
-
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	stdoutTrace "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	"go.opentelemetry.io/otel/propagation"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/trace"
 )
+
+const OtelTracerName = "terrarium-tracer"
 
 const (
 	defaultEndpoint = "0.0.0.0:3001"
@@ -38,20 +31,20 @@ var rootCmd = &cobra.Command{
 	Long:  "Runs backend that exposes Terrarium Services",
 }
 
-func InitOTELTracer() (*sdktrace.TracerProvider, error) {
-	exporter, err := stdoutTrace.New(stdoutTrace.WithPrettyPrint())
-	if err != nil {
-		return nil, err
-	}
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithBatcher(exporter),
-	)
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+// func InitOTELTracer() (*sdktrace.TracerProvider, error) {
+// 	exporter, err := stdoutTrace.New(stdoutTrace.WithPrettyPrint())
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	tp := sdktrace.NewTracerProvider(
+// 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+// 		sdktrace.WithBatcher(exporter),
+// 	)
+// 	otel.SetTracerProvider(tp)
+// 	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
 
-	return tp, nil
-}
+// 	return tp, nil
+// }
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&endpoint, "endpoint", "e", defaultEndpoint, "Endpoint")
@@ -64,6 +57,7 @@ func init() {
 }
 
 func startGRPCService(name string, service services.Service) {
+
 	log.Printf("Starting %s", name)
 
 	listener, err := net.Listen("tcp4", endpoint)
@@ -83,42 +77,6 @@ func startGRPCService(name string, service services.Service) {
 		log.Fatalf("Failed: %v", err)
 	}
 
-	traceProvider, err := InitOTELTracer()
-
-	if err != nil {
-		log.Fatalf("Failed to initiate OTEL Tracer: %v", err)
-	}
-
-	if traceProvider != nil {
-		log.Printf("Successfully initiated OTEL Tracer...")
-	}
-
-	tracer := otel.Tracer("test-tracer")
-
-	// Attributes represent additional key-value descriptors that can be bound
-	// to a metric observer or recorder.
-	commonAttrs := []attribute.KeyValue{
-		attribute.String("attrA", "chocolate"),
-		attribute.String("attrB", "raspberry"),
-		attribute.String("attrC", "vanilla"),
-	}
-
-	// work begins
-	ctx, span := tracer.Start(
-		context.TODO(),
-		"CollectorExporter-Example",
-		trace.WithAttributes(commonAttrs...))
-	defer span.End()
-	for i := 0; i < 10; i++ {
-		_, iSpan := tracer.Start(ctx, fmt.Sprintf("Sample-%d", i))
-		log.Printf("Doing really hard work (%d / 10)\n", i+1)
-
-		<-time.After(time.Second)
-		iSpan.End()
-	}
-
-	log.Printf("Done!")
-
 }
 
 func startRESTAPIService(name, mountPath string, rootHandler restapi.RESTAPIHandler) {
@@ -126,41 +84,6 @@ func startRESTAPIService(name, mountPath string, rootHandler restapi.RESTAPIHand
 	log.Println(fmt.Sprintf("Listening on %s", endpoint))
 	log.Fatal(http.ListenAndServe(endpoint, rootHandler.GetHttpHandler(mountPath)))
 
-	traceProvider, err := InitOTELTracer()
-
-	if err != nil {
-		log.Fatalf("Failed to initiate OTEL Tracer: %v", err)
-	}
-
-	if traceProvider != nil {
-		log.Printf("Successfully initiated OTEL Tracer...")
-	}
-
-	tracer := otel.Tracer("test-tracer")
-
-	// Attributes represent additional key-value descriptors that can be bound
-	// to a metric observer or recorder.
-	commonAttrs := []attribute.KeyValue{
-		attribute.String("attrA", "chocolate"),
-		attribute.String("attrB", "raspberry"),
-		attribute.String("attrC", "vanilla"),
-	}
-
-	// work begins
-	ctx, span := tracer.Start(
-		context.TODO(),
-		"CollectorExporter-Example",
-		trace.WithAttributes(commonAttrs...))
-	defer span.End()
-	for i := 0; i < 10; i++ {
-		_, iSpan := tracer.Start(ctx, fmt.Sprintf("Sample-%d", i))
-		log.Printf("Doing really hard work (%d / 10)\n", i+1)
-
-		<-time.After(time.Second)
-		iSpan.End()
-	}
-
-	log.Printf("Done!")
 }
 
 // Execute root command
--- go.mod
+++ go.mod
@@ -7,27 +7,25 @@ require (
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
 	github.com/spf13/cobra v1.5.0
-	google.golang.org/grpc v1.48.0
+	go.opentelemetry.io/otel v1.11.2
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.11.2
+	go.opentelemetry.io/otel/sdk v1.11.2
+	google.golang.org/grpc v1.52.0
 	google.golang.org/protobuf v1.28.1
 	gopkg.in/errgo.v2 v2.1.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	go.opentelemetry.io/otel v1.11.2 // indirect
-	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.11.2 // indirect
-	go.opentelemetry.io/otel/sdk v1.11.2 // indirect
 	go.opentelemetry.io/otel/trace v1.11.2 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20221118155620-16455021b5e6 // indirect
 )
