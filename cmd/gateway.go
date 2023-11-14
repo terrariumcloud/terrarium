8dbec20b75889456fc09bb2e9a772b05154d4f25
Ilija Dimitrovski <36102926+ilijad1@users.noreply.github.com>
2023-11-14 15:47:34 +0100

    update GRPC service name

--- cmd/gateway.go
+++ cmd/gateway.go
@@ -26,7 +26,7 @@ func init() {
 	gatewayCmd.Flags().StringVarP(&version_manager.VersionManagerEndpoint, "version-manager", "", version_manager.DefaultVersionManagerEndpoint, "GRPC Endpoint for Version Manager Service")
 	gatewayCmd.Flags().StringVarP(&storage.StorageServiceEndpoint, "storage", "", storage.DefaultStorageServiceDefaultEndpoint, "GRPC Endpoint for Storage Service")
 	gatewayCmd.Flags().StringVarP(&tag_manager.TagManagerEndpoint, "tag-manager", "", tag_manager.DefaultTagManagerEndpoint, "GRPC Endpoint for Tag Service")
-	gatewayCmd.Flags().StringVarP(&release.ReleaseServiceEndpoint, "tag", "", release.DefaultReleaseServiceEndpoint, "GRPC Endpoint for Release Service")
+	gatewayCmd.Flags().StringVarP(&release.ReleaseServiceEndpoint, "releases", "", release.DefaultReleaseServiceEndpoint, "GRPC Endpoint for Release Service")
 }
 
 func runGateway(cmd *cobra.Command, args []string) {
