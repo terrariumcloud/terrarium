ae68284f1aa897e9e5e158d79a6cf498134e007a
Pranav D K <pranavdk@synamedia.com>
2023-11-27 15:56:25 +0530

    Adding Image attribution

--- internal/restapi/browse/frontend/src/pages/main/Main.tsx
+++ internal/restapi/browse/frontend/src/pages/main/Main.tsx
@@ -41,7 +41,7 @@ function Main() {
                         To discover the list of available module and their versions, Terrarium comes with module discovery
                         and search capabilities which we hopefully made friendly enough to be of use.
                     </Typography>
-                    <br/>
+                    <br />
                     <Button variant="contained" fullWidth component={RouterLink} to="terraform-modules">Browse for modules</Button>
                 </Grid>
                 <Grid item xs={5}>
@@ -51,9 +51,12 @@ function Main() {
 
             <Divider variant="middle" />
 
-            <Grid container direction="row" alignItems="strech" spacing={5} style={{ marginTop: "1em", marginBottom: "1em"  }}>
+            <Grid container direction="row" alignItems="strech" spacing={5} style={{ marginTop: "1em", marginBottom: "1em" }}>
                 <Grid item xs={5}>
                     <img style={{ borderRadius: "10px" }} src={providersImage} width="100%" alt="Work in progress support for providers" />
+                    <Typography variant="h6" color="gray" noWrap style={{ fontSize: "8px", textAlign: 'center' }}>
+                        Image by <a className='nodecorlink' href="https://www.freepik.com/free-vector/vector-cartoon-illustration-process-construction-buildings_1215826.htm">vectorpocket</a> on Freepik
+                    </Typography>
                 </Grid>
                 <Grid item xs={7} >
                     <Typography variant="h5" align='right' gutterBottom>Providers</Typography>
@@ -69,13 +72,16 @@ function Main() {
                 <Grid item xs={7} >
                     <Typography variant="h5" align='left' gutterBottom>Releases</Typography>
                     <Typography variant="body2" gutterBottom>
-                        Keep track of latest releases. It will include all terraform releases but also bundles and more. 
+                        Keep track of latest releases. It will include all terraform releases but also bundles and more.
                     </Typography>
-                    <br/>
+                    <br />
                     <Button variant="contained" fullWidth component={RouterLink} to="releases">Browse Recent Releases</Button>
                 </Grid>
                 <Grid item xs={5}>
                     <img src={releasesImage} width="100%" alt="Terraform Releases illustration" />
+                    <Typography variant="h6" color="gray" noWrap style={{ fontSize: "8px", textAlign: 'center' }}>
+                        Image by <a className='nodecorlink' href="https://www.freepik.com/free-vector/hand-drawn-transport-truck-delivery-man_19962585.htm">Freepik</a>
+                    </Typography>
                 </Grid>
             </Grid>
         </>
