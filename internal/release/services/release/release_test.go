4cb6892d7a730cd045f31277b2732271108eca73
Adam Charrett <adcharre@synamedia.com>
2023-11-30 19:26:34 +0000

    Add test and build on PR

--- internal/release/services/release/release_test.go
+++ internal/release/services/release/release_test.go
@@ -56,6 +56,7 @@ func Test_RegisterReleaseWithServer(t *testing.T) {
 
 		db := &mocks.DynamoDB{
 			DescribeTableErrors: []error{errors.New("some error")},
+			CreateTableError:    errors.New("some error"),
 		}
 
 		rs := &ReleaseService{
