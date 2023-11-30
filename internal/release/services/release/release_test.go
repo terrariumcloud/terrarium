4a886fcb313cce334c13fdd36cb762bfa737e52e
Adam Charrett <adcharre@synamedia.com>
2023-11-30 19:30:45 +0000

    Update release_test.go

--- internal/release/services/release/release_test.go
+++ internal/release/services/release/release_test.go
@@ -75,8 +75,8 @@ func Test_RegisterReleaseWithServer(t *testing.T) {
 			t.Errorf("Expected 1 call to DescribeTable, got %v.", db.DescribeTableInvocations)
 		}
 
-		if db.CreateTableInvocations != 0 {
-			t.Errorf("Expected 0 calls to CreateTable, got %v.", db.CreateTableInvocations)
+		if db.CreateTableInvocations != 1 {
+			t.Errorf("Expected 1 calls to CreateTable, got %v.", db.CreateTableInvocations)
 		}
 	})
 }
