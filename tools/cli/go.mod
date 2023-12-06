a6b9f9b236e4d7245b3dd625ef921580d89dd760
Adam Charrett <adcharre@synamedia.com>
2023-12-06 21:27:15 +0000

    Unit tests

--- tools/cli/go.mod
+++ tools/cli/go.mod
@@ -4,19 +4,23 @@ go 1.21.4
 
 require (
 	github.com/spf13/cobra v1.8.0
+	github.com/stretchr/testify v1.8.4
 	github.com/terrariumcloud/terrarium v0.0.69
 	google.golang.org/grpc v1.59.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 replace github.com/terrariumcloud/terrarium/pkg/terrarium/module v0.0.69 => ../../pkg/terrarium/module
--- tools/cli/pkg/module/publish_test.go
+++ tools/cli/pkg/module/publish_test.go
@@ -3,6 +3,7 @@ package module
 import (
 	"context"
 	"errors"
+	"github.com/stretchr/testify/require"
 	"github.com/terrariumcloud/terrarium/pkg/terrarium/module"
 	"google.golang.org/grpc"
 	"io"
@@ -12,7 +13,7 @@ import (
 
 func TestPublish(t *testing.T) {
 	type args struct {
-		client   module.PublisherClient
+		client   *MockPublisherClient
 		source   io.Reader
 		metadata Metadata
 	}
@@ -20,7 +21,10 @@ func TestPublish(t *testing.T) {
 		registerCalls                      int
 		beginVersionCalls                  int
 		registerContainerDependenciesCalls int
+		uploadSourceZipCalls               int
+		uploadSourceZipSendCalls           int
 		endVersionCalls                    int
+		endVersionRequest                  *module.EndVersionRequest
 	}
 	tests := []struct {
 		name     string
@@ -32,7 +36,13 @@ func TestPublish(t *testing.T) {
 			name: "Success",
 			args: args{
 				client: &MockPublisherClient{
-					uploadSourceZipResponse: &MockPublisher_UploadSourceZipClient{sendError: errors.New("not implemented")},
+					uploadSourceZipResponse: &MockPublisher_UploadSourceZipClient{
+						sendError:         nil,
+						closeAndRecvError: nil,
+						closeAndRecvResponse: &module.Response{
+							Message: "done",
+						},
+					},
 				},
 				source: strings.NewReader("test"),
 				metadata: Metadata{
@@ -43,14 +53,158 @@ func TestPublish(t *testing.T) {
 					Maturity:    module.Maturity_PLANNING,
 				},
 			},
-			expected: expected{},
-			wantErr:  false,
+			expected: expected{
+				registerCalls:                      1,
+				beginVersionCalls:                  1,
+				registerContainerDependenciesCalls: 0,
+				uploadSourceZipCalls:               1,
+				uploadSourceZipSendCalls:           1,
+				endVersionCalls:                    1,
+				endVersionRequest: &module.EndVersionRequest{
+					Module: &module.Module{
+						Name:    "org/test/provider",
+						Version: "1.2.4",
+					},
+					Action: module.EndVersionRequest_PUBLISH,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Fail during register()",
+			args: args{
+				client: &MockPublisherClient{
+					registerError: errors.New("failed"),
+				},
+				source: strings.NewReader("test"),
+				metadata: Metadata{
+					Name:        "org/test/provider",
+					Version:     "1.2.4",
+					Description: "A test",
+					Source:      "http://test.com/test",
+					Maturity:    module.Maturity_PLANNING,
+				},
+			},
+			expected: expected{
+				registerCalls:                      1,
+				beginVersionCalls:                  0,
+				registerContainerDependenciesCalls: 0,
+				uploadSourceZipCalls:               0,
+				uploadSourceZipSendCalls:           0,
+				endVersionCalls:                    0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Fail during register()",
+			args: args{
+				client: &MockPublisherClient{
+					beginVersionError: errors.New("failed"),
+				},
+				source: strings.NewReader("test"),
+				metadata: Metadata{
+					Name:        "org/test/provider",
+					Version:     "1.2.4",
+					Description: "A test",
+					Source:      "http://test.com/test",
+					Maturity:    module.Maturity_PLANNING,
+				},
+			},
+			expected: expected{
+				registerCalls:                      1,
+				beginVersionCalls:                  1,
+				registerContainerDependenciesCalls: 0,
+				uploadSourceZipCalls:               0,
+				uploadSourceZipSendCalls:           0,
+				endVersionCalls:                    0,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Fail at upload",
+			args: args{
+				client: &MockPublisherClient{
+					uploadSourceZipError: errors.New("failed"),
+				},
+				source: strings.NewReader("test"),
+				metadata: Metadata{
+					Name:        "org/test/provider",
+					Version:     "1.2.4",
+					Description: "A test",
+					Source:      "http://test.com/test",
+					Maturity:    module.Maturity_PLANNING,
+				},
+			},
+			expected: expected{
+				registerCalls:                      1,
+				beginVersionCalls:                  1,
+				registerContainerDependenciesCalls: 0,
+				uploadSourceZipCalls:               1,
+				uploadSourceZipSendCalls:           0,
+				endVersionCalls:                    1,
+				endVersionRequest: &module.EndVersionRequest{
+					Module: &module.Module{
+						Name:    "org/test/provider",
+						Version: "1.2.4",
+					},
+					Action: module.EndVersionRequest_DISCARD,
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Fail during upload send()",
+			args: args{
+				client: &MockPublisherClient{
+					uploadSourceZipResponse: &MockPublisher_UploadSourceZipClient{
+						sendError:         errors.New("failed"),
+						closeAndRecvError: nil,
+						closeAndRecvResponse: &module.Response{
+							Message: "done",
+						},
+					},
+				},
+				source: strings.NewReader("test"),
+				metadata: Metadata{
+					Name:        "org/test/provider",
+					Version:     "1.2.4",
+					Description: "A test",
+					Source:      "http://test.com/test",
+					Maturity:    module.Maturity_PLANNING,
+				},
+			},
+			expected: expected{
+				registerCalls:                      1,
+				beginVersionCalls:                  1,
+				registerContainerDependenciesCalls: 0,
+				uploadSourceZipCalls:               1,
+				uploadSourceZipSendCalls:           1,
+				endVersionCalls:                    1,
+				endVersionRequest: &module.EndVersionRequest{
+					Module: &module.Module{
+						Name:    "org/test/provider",
+						Version: "1.2.4",
+					},
+					Action: module.EndVersionRequest_DISCARD,
+				},
+			},
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := Publish(tt.args.client, tt.args.source, tt.args.metadata); (err != nil) != tt.wantErr {
 				t.Errorf("Publish() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			require.Equal(t, tt.expected.registerCalls, tt.args.client.registerCalls)
+			require.Equal(t, tt.expected.beginVersionCalls, tt.args.client.beginVersionCalls)
+			require.Equal(t, tt.expected.uploadSourceZipCalls, tt.args.client.uploadSourceZipCalls)
+			require.Equal(t, tt.expected.endVersionCalls, tt.args.client.endVersionCalls)
+			if tt.expected.endVersionRequest != nil {
+				require.Equal(t, tt.expected.endVersionRequest, tt.args.client.endVersionRequest)
+			}
+			if tt.args.client.uploadSourceZipResponse != nil {
+				require.Equal(t, tt.expected.uploadSourceZipSendCalls, tt.args.client.uploadSourceZipResponse.sendCalls)
 			}
 		})
 	}
@@ -71,8 +225,9 @@ type MockPublisherClient struct {
 	registerContainerDependenciesResponse *module.Response
 	uploadSourceZipCalls                  int
 	uploadSourceZipError                  error
-	uploadSourceZipResponse               module.Publisher_UploadSourceZipClient
+	uploadSourceZipResponse               *MockPublisher_UploadSourceZipClient
 	endVersionCalls                       int
+	endVersionRequest                     *module.EndVersionRequest
 	endVersionError                       error
 	endVersionResponse                    *module.Response
 	publishTagCalls                       int
@@ -80,52 +235,55 @@ type MockPublisherClient struct {
 	publishTagResponse                    *module.Response
 }
 
-func (m MockPublisherClient) Register(ctx context.Context, in *module.RegisterModuleRequest, opts ...grpc.CallOption) (*module.Response, error) {
+func (m *MockPublisherClient) Register(ctx context.Context, in *module.RegisterModuleRequest, opts ...grpc.CallOption) (*module.Response, error) {
 	m.registerCalls++
 	return m.registerResponse, m.registerError
 }
 
-func (m MockPublisherClient) BeginVersion(ctx context.Context, in *module.BeginVersionRequest, opts ...grpc.CallOption) (*module.Response, error) {
+func (m *MockPublisherClient) BeginVersion(ctx context.Context, in *module.BeginVersionRequest, opts ...grpc.CallOption) (*module.Response, error) {
 	m.beginVersionCalls++
 	return m.beginVersionResponse, m.beginVersionError
 }
 
-func (m MockPublisherClient) RegisterModuleDependencies(ctx context.Context, in *module.RegisterModuleDependenciesRequest, opts ...grpc.CallOption) (*module.Response, error) {
+func (m *MockPublisherClient) RegisterModuleDependencies(ctx context.Context, in *module.RegisterModuleDependenciesRequest, opts ...grpc.CallOption) (*module.Response, error) {
 	m.registerModuleDependenciesCalls++
 	return m.registerModuleDependenciesResponse, m.registerModuleDependenciesError
 }
 
-func (m MockPublisherClient) RegisterContainerDependencies(ctx context.Context, in *module.RegisterContainerDependenciesRequest, opts ...grpc.CallOption) (*module.Response, error) {
+func (m *MockPublisherClient) RegisterContainerDependencies(ctx context.Context, in *module.RegisterContainerDependenciesRequest, opts ...grpc.CallOption) (*module.Response, error) {
 	m.registerContainerDependenciesCalls++
 	return m.registerContainerDependenciesResponse, m.registerContainerDependenciesError
 }
 
-func (m MockPublisherClient) UploadSourceZip(ctx context.Context, opts ...grpc.CallOption) (module.Publisher_UploadSourceZipClient, error) {
+func (m *MockPublisherClient) UploadSourceZip(ctx context.Context, opts ...grpc.CallOption) (module.Publisher_UploadSourceZipClient, error) {
 	m.uploadSourceZipCalls++
 	return m.uploadSourceZipResponse, m.uploadSourceZipError
 }
 
-func (m MockPublisherClient) EndVersion(ctx context.Context, in *module.EndVersionRequest, opts ...grpc.CallOption) (*module.Response, error) {
+func (m *MockPublisherClient) EndVersion(ctx context.Context, in *module.EndVersionRequest, opts ...grpc.CallOption) (*module.Response, error) {
 	m.endVersionCalls++
+	m.endVersionRequest = in
 	return m.endVersionResponse, m.endVersionError
 }
 
-func (m MockPublisherClient) PublishTag(ctx context.Context, in *module.PublishTagRequest, opts ...grpc.CallOption) (*module.Response, error) {
+func (m *MockPublisherClient) PublishTag(ctx context.Context, in *module.PublishTagRequest, opts ...grpc.CallOption) (*module.Response, error) {
 	m.publishTagCalls++
 	return m.publishTagResponse, m.publishTagError
 }
 
 type MockPublisher_UploadSourceZipClient struct {
 	grpc.ClientStream
+	sendCalls            int
 	sendError            error
 	closeAndRecvError    error
 	closeAndRecvResponse *module.Response
 }
 
-func (m MockPublisher_UploadSourceZipClient) Send(request *module.UploadSourceZipRequest) error {
+func (m *MockPublisher_UploadSourceZipClient) Send(request *module.UploadSourceZipRequest) error {
+	m.sendCalls++
 	return m.sendError
 }
 
-func (m MockPublisher_UploadSourceZipClient) CloseAndRecv() (*module.Response, error) {
+func (m *MockPublisher_UploadSourceZipClient) CloseAndRecv() (*module.Response, error) {
 	return m.closeAndRecvResponse, m.closeAndRecvError
 }
