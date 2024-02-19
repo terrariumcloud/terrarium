cfa0cf457415f4bb5fa6d060cd28ccb30fb4b131
A K Srivastava <55885026+anjanikshree12@users.noreply.github.com>
2024-02-19 12:16:42 +0530

    Implementation of the provider registry protocol v1 (#65)

---
+++ cmd/rest_providers_v1.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	providersv1 "github.com/terrariumcloud/terrarium/internal/restapi/providers/v1"
+
+	"github.com/spf13/cobra"
+)
+
+var mountPathProviders string
+
+var providersV1Cmd = &cobra.Command{
+	Use:   "providers.v1",
+	Short: "Starts the Terrarium REST API service implementing a read only version of the provider.v1 registry protocol",
+	Long:  "Runs the Terrarium REST server for the implementation of the provider.v1 protocol",
+	Run:   runRESTProvidersV1Server,
+}
+
+func init() {
+	providersV1Cmd.Flags().StringVarP(
+		&mountPathProviders,
+		"mount-path",
+		"m",
+		"providers",
+		"Mount path for the rest API server used to process request relative to a particular URL in a reverse proxy type setup",
+	)
+	rootCmd.AddCommand(providersV1Cmd)
+}
+
+func runRESTProvidersV1Server(cmd *cobra.Command, args []string) {
+	restAPIServer := providersv1.New()
+	startRESTAPIService("rest-providers-v1", mountPathProviders, restAPIServer)
+}
