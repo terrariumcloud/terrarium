868924ace38d4af4e2150fba1cc99bfbd27f4c02
Anjani Kumar Srivastava <anjani@anjanivm.com>
2024-04-30 03:51:51 +0000

    Revert "fix"
    This reverts commit ca2601f527c59a97b9c6298d01ce6f751caf041c.

--- internal/provider/services/storage/storage_test.go
+++ internal/provider/services/storage/storage_test.go
@@ -144,7 +144,7 @@ func Test_DownloadProviderSourceZip(t *testing.T) {
 	})
 
 	t.Run("when Send fails", func(t *testing.T) {
-		var length int64 = 50000
+		var length int64 = 10000
 		buf := &ClosingBuffer{bytes.NewBuffer(make([]byte, length))}
 
 		s3Client := &mocks2.S3{GetObjectOut: &s3.GetObjectOutput{Body: buf, ContentLength: length}}
@@ -181,7 +181,7 @@ func Test_DownloadShasum(t *testing.T) {
 	t.Parallel()
 
 	t.Run("when shasum file is downloaded", func(t *testing.T) {
-		var length int64 = 50000
+		var length int64 = 1000
 		buf := &ClosingBuffer{bytes.NewBuffer(make([]byte, length))}
 
 		s3Client := &mocks2.S3{GetObjectOut: &s3.GetObjectOutput{Body: buf, ContentLength: length}}
@@ -242,7 +242,7 @@ func Test_DownloadShasum(t *testing.T) {
 	})
 
 	t.Run("when Send fails", func(t *testing.T) {
-		var length int64 = 50000
+		var length int64 = 1000
 		buf := &ClosingBuffer{bytes.NewBuffer(make([]byte, length))}
 
 		s3Client := &mocks2.S3{GetObjectOut: &s3.GetObjectOutput{Body: buf, ContentLength: length}}
@@ -279,7 +279,7 @@ func Test_DownloadShasumSignature(t *testing.T) {
 	t.Parallel()
 
 	t.Run("when shasum signature file is downloaded", func(t *testing.T) {
-		var length int64 = 50000
+		var length int64 = 1000
 		buf := &ClosingBuffer{bytes.NewBuffer(make([]byte, length))}
 
 		s3Client := &mocks2.S3{GetObjectOut: &s3.GetObjectOutput{Body: buf, ContentLength: length}}
@@ -340,7 +340,7 @@ func Test_DownloadShasumSignature(t *testing.T) {
 	})
 
 	t.Run("when Send fails", func(t *testing.T) {
-		var length int64 = 50000
+		var length int64 = 1000
 		buf := &ClosingBuffer{bytes.NewBuffer(make([]byte, length))}
 
 		s3Client := &mocks2.S3{GetObjectOut: &s3.GetObjectOutput{Body: buf, ContentLength: length}}
